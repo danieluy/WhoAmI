ed0daefe98f1eb41191f10e8acc37f17e563819a
danieluy <danielsosa.dev@gmail.com>
2017-01-17 17:43:44 -0300

    Working to solve the instantiation problems

--- my_modules/game.js
+++ my_modules/game.js
@@ -1,30 +1,53 @@
 "use strict";
 
 const Game = {
-  players: {},
+  id: null,
+
+  player: {
+    players: {},
+    add: function (player) {
+      if (this.players.hasOwnProperty(player.id))
+        throw new Error('Player already exists');
+      else
+        this.players[player.id] = player;
+    },
+    get: function (player_id) {
+      return this.players[player_id];
+    },
+    delete: function (player_id) {
+      delete this.players[player_id];
+    }
+  },
+  character: {
+    characters: {},
+    add: function (character) {
+      if (this.characters.hasOwnProperty(character.id))
+        throw new Error('Character already exists');
+      else
+        this.characters[character.id] = character;
+    },
+    get: function (character_id) {
+      return this.characters[character_id];
+    },
+    delete: function (character_id) {
+      delete this.characters[character_id];
+    }
+  }
+}
+
+Game.prototype.socket = {
   sockets: {},
-  characters: {},
-  addSocket: function (socket) {
+  add: function (socket) {
     if (this.sockets.hasOwnProperty(socket.id))
-      throw 'Socket already exists';
+      throw new Error('Socket already exists');
     else
       this.sockets[socket.id] = socket;
   },
-  addPlayer: function (player) {
-    if (this.players.hasOwnProperty(player.id))
-      throw 'Player already exists';
-    else
-      this.players[player.id] = player;
+  get: function (socket_id) {
+    return this.sockets[socket_id];
   },
-  addCharacter: function (character) {
-    if (this.characters.hasOwnProperty(character.id))
-      throw 'Character already exists';
-    else
-      this.characters[character.id] = character;
-  },
-  emitUpdatePlayers: function () {
-    for (key in this.players)
-      this.sockets[key].emit('updatePlayers', this.players);
+  delete: function (socket_id) {
+    delete this.sockets[socket_id];
   }
 }
 
--- test/whoami-test.js
+++ test/whoami-test.js
@@ -1,7 +1,26 @@
 const assert = require('chai').assert;
 const expect = require('chai').expect;
 
-describe('player.js', function () {
+describe('Object', function(){
+  it.only('????????????', function(){
+    const Game = require('../my_modules/game.js');
+    const Player = require('../my_modules/player.js');
+    console.log(Game.player.players);
+    let p = Object.create(Player);
+    p.id = 'someId'
+    Game.player.add(p);
+    console.log(Game.player.players);
+
+    let g = Object.create(Game);
+    let p1 = Object.create(Player);
+    p1.id = 'someOtherId'
+    g.player.add(p1)
+
+    console.log(Game.player.players);
+  })
+})
+
+describe('Player', function () {
   let Player;
 
   before(function () {
@@ -15,26 +34,58 @@ describe('player.js', function () {
 
 });
 
-describe('game.js', function () {
+describe('Game', function () {
   let Game;
+  let Player;
 
   before(function () {
     Game = require('../my_modules/game.js');
+    Player = require('../my_modules/player.js');
   });
 
-  it('should instantiate a Game', function () {
+  it('Instantiate a Game', function () {
     const g = Object.create(Game);
     assert.isTrue(Game.isPrototypeOf(g));
   });
 
-  it('should add a Game', function () {
+  it('Add and retrieve a Game', function () {
+    const games = {};
+    const g1 = Object.create(Game);
+    g1.id = 'someid';
+    games[g1.id] = g1;
+    const g2 = games.someid;
+    g1.someKey = 'some value';
+    assert.equal(g1.someKey, g2.someKey);
+  });
+
+  it('Add and retrieve a Player', function () {
     const g = Object.create(Game);
-    assert.isTrue(Game.isPrototypeOf(g));
+    const p1 = Object.create(Player);
+    p1.id = 'someId'
+    g.player.add(p1);
+    const p2 = g.player.get('someId');
+    p2.username = 'some name';
+    assert.equal(p1.username, p2.username);
+  });
+
+  it('Should throw "Player already exists"', function () {
+    const g = Object.create(Game);
+    const p1 = Object.create(Player);
+    const p2 = Object.create(Player);
+    p1.id = 'someId';
+    p2.id = 'someId';
+    g.player.add(p1);
+    try {
+      g.player.add(p2);
+    }
+    catch (error) {
+      assert.equal(error.message, "Player already exists");
+    }
   });
 
 });
 
-describe('character.js', function () {
+describe('Character', function () {
   let Character;
 
   before(function () {
